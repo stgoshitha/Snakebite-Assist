2b3dbbf600b4477a3279145a9a534aa50d8e5b2a
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-03-17 23:18:47 +0530

    feat: initialize project with backend server and database configuration and setup frontend with Tailwind CSS

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express') 
+const connectDB = require('./config/db');
+const cookieparser = require('cookie-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+app.get('/', (req, res) => {
+    res.send('server start!')
+  })
+  
+// Routes
+
+
+
+// Connect to the MongoDB database
+connectDB();
+
+//Server
+try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+      console.error("Failed to start the server:", error);
+}
