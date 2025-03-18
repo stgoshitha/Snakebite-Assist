35a60ad0e0d015d3422705d58d025daa43178416
tharushika2002 <sttharuu123@gmail.com>
2025-03-18 08:10:47 +0530

    feat: add user model in user.js

---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    enum: ["user", "hospital", "admin", "superadmin"],
+    default: "user"
+  },
+  password: {
+    type: String,
+    required: true
+  },
+},{timestamps:true});
+
+const User = mongoose.model("user", userSchema);  
+module.exports = User;
