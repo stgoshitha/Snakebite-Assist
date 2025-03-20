394031fefc51b47d625bb5df9a36946033d89c06
tharushika2002 <sttharuu123@gmail.com>
2025-03-20 06:30:14 +0530

    change code test components, layout, page, redux, and service files

---
+++ frontend/src/components/testComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testComponent = () => {
+  return (
+    <div>testComponent</div>
+  )
+}
+
+export default testComponent
---
+++ frontend/src/redux/testRedux.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const testRedux = () => {
+  return (
+    <div>testRedux</div>
+  )
+}
+
+export default testRedux
