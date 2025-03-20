ca0d126fa4f33e8b67996cc3cbe4d61262f9de34
tharushika2002 <sttharuu123@gmail.com>
2025-03-20 12:33:12 +0530

    add routing and layout components for Admin, Hospital, User, and Public sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout";
+import HospitalLayout from "./layouts/HospitalLayout";
+import UserLayout from "./layouts/UserLayout";
+import PublicLayout from "./layouts/PublicLayout";
+import Home from "./pages/Home";
+import AdminDashboardUsers from "./pages/AdminDashboardUsers";
+import HospitalDashboard from "./pages/HospitalDashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 
 function App() {
   return (
-    <div>
-      <h1>SnakeBite Assits</h1>
-    </div>
-  )
+    <Router>
+      <div>
+        <Routes>
+        <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<Home />} />
+            <Route path='adminUsers' element={<AdminDashboardUsers />} />
+          </Route>
+
+          <Route path='/' element={<UserLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+
+          <Route path="/hospital" element={<HospitalLayout />}>
+            <Route index element={<Home />} />
+            <Route path="hospitaldash" element={<HospitalDashboard />} />
+          </Route>
+
+          <Route path='/' element={<PublicLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
