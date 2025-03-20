c589f7d05acf8c09c5c0b2ca6b7154699592b074
tharushika2002 <sttharuu123@gmail.com>
2025-03-20 06:12:52 +0530

    refactor user schema to include isBlocked field

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "hospital", "admin", "superadmin"],
+      default: "user",
+    },
+    isBlocked: { 
+      type: Boolean, 
+      default: false 
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  role: {
-    type: String,
-    enum: ["user", "hospital", "admin", "superadmin"],
-    default: "user"
-  },
-  password: {
-    type: String,
-    required: true
-  },
-},{timestamps:true});
+  { timestamps: true }
+);
 
-const User = mongoose.model("user", userSchema);  
+const User = mongoose.model("user", userSchema);
 module.exports = User;
