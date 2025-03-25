9a3a6423c7ee52dd78eb597c610f80dd388c51f4
tharushika2002 <156164194+Jeewapadma@users.noreply.github.com>
2025-03-25 17:59:28 +0530

    add hospital model file

---
+++ backend/models/Hospital.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const HospitalSchema = new mongoose.Schema(
+  {
+    hospitalName: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true,
+    },
+    latitude: { type: Number },
+    longitude: { type: Number },
+    is24hrService: { 
+      type: Boolean, 
+      default: false,
+    },
+    workingHours: [
+      {
+        day: { type: String, required: true }, 
+        open: { type: String, required: true }, 
+        close: { type: String, required: true },
+      },
+    ],
+    proofCertificate: { type: String, required: true },
+    hospitalImages: { type: [String] },
+    user: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'user',
+      required: true 
+    }
+  },
+  { timestamps: true } 
+);
+
+const Hospital = mongoose.model('hospital', HospitalSchema);
+module.exports = Hospital;
