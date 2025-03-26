ad6a698b9617ed230c48a373626a4fbe438b70cb
tharushika2002 <156164194+Jeewapadma@users.noreply.github.com>
2025-03-26 10:50:35 +0530

    Set up hospital routes with authentication and authorization

---
+++ backend/routes/hospitalRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hospitalRouter = express.Router();
+const {createHospital,getAllHospitalApproved,getAllHospitalNotApproved,getOneHospital,approveHospital,updateHospital,deleteHospital} = require('../controllers/hospitalController');
+const {authenticateHospital,isAdminOrSuperAdmin} = require('../middleware/verifyToken')
+
+hospitalRouter.post('/createHospital', authenticateHospital, createHospital);
+hospitalRouter.get('/getAllHospitalApproved', isAdminOrSuperAdmin, getAllHospitalApproved);
+hospitalRouter.get('/getAllHospitalNotApproved', isAdminOrSuperAdmin, getAllHospitalNotApproved);
+hospitalRouter.get('/getOneHospital/:id', authenticateHospital, getOneHospital);
+hospitalRouter.put('/updateHospital/:id', authenticateHospital, updateHospital);
+hospitalRouter.delete('/deleteHospital/:id', authenticateHospital, deleteHospital);
+hospitalRouter.put('/hospitals/:id/approve', isAdminOrSuperAdmin, approveHospital);
+
+module.exports = hospitalRouter;
