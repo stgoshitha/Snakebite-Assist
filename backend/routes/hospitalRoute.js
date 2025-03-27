f38d5c68f5f0371e12fde0102da27612539d0e6b
tharushika2002 <156164194+Jeewapadma@users.noreply.github.com>
2025-03-27 10:36:18 +0530

    Refactor hospital routes for generic authentication

--- backend/routes/hospitalRoute.js
+++ backend/routes/hospitalRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const hospitalRouter = express.Router();
-const {createHospital,getAllHospitalApproved,getAllHospitalNotApproved,getOneHospital,approveHospital,updateHospital,deleteHospital} = require('../controllers/hospitalController');
-const {authenticateHospital,isAdminOrSuperAdmin} = require('../middleware/verifyToken')
+const {createHospital,getAllHospitalApproved,getAllHospitalNotApproved,getUserHospital,approveHospital,updateHospital,deleteHospital} = require('../controllers/hospitalController');
+const {authenticate,isAdminOrSuperAdmin} = require('../middleware/verifyToken')
 
-hospitalRouter.post('/createHospital', authenticateHospital, createHospital);
+hospitalRouter.post('/createHospital', authenticate, createHospital);
 hospitalRouter.get('/getAllHospitalApproved', isAdminOrSuperAdmin, getAllHospitalApproved);
 hospitalRouter.get('/getAllHospitalNotApproved', isAdminOrSuperAdmin, getAllHospitalNotApproved);
-hospitalRouter.get('/getOneHospital/:id', authenticateHospital, getOneHospital);
-hospitalRouter.put('/updateHospital/:id', authenticateHospital, updateHospital);
-hospitalRouter.delete('/deleteHospital/:id', authenticateHospital, deleteHospital);
+hospitalRouter.get('/getUserHospital', authenticate, getUserHospital);
+hospitalRouter.put('/updateHospital/:id',authenticate, updateHospital);
+hospitalRouter.delete('/deleteHospital/:id',authenticate, deleteHospital);
 hospitalRouter.put('/approve/:id', isAdminOrSuperAdmin, approveHospital);
 
 module.exports = hospitalRouter;
