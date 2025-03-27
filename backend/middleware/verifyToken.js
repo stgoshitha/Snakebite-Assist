ad6fe4df483c370cd4d9ed49849ab7d3ceb6f5a8
tharushika2002 <156164194+Jeewapadma@users.noreply.github.com>
2025-03-27 10:34:08 +0530

    Refactor authentication middleware

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -42,40 +42,28 @@ const isSuperAdmin = async (req, res, next) => {
   }
 };
 
-// Middleware to authenticate and authorize hospital users
-const authenticateHospital = (req, res, next) => {
-  
-  const authHeader = req.header('Authorization');
-  //console.log("Authorization Header:", authHeader);  
-  
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided or incorrect format' });
-  }
-
-  const token = authHeader.split(' ')[1];
-  //console.log("Extracted Token:", token);
-
-  if (!token) {
-    return res.status(401).json({ message: 'Token is missing' });
-  }
-
+const authenticate = async (req, res, next) => {
   try {
+    const token = req.header('Authorization').replace('Bearer ', ''); 
+    //console.log(token);
+  
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //console.log("Decoded Token:", decoded);
-
-    req.user = decoded;
-
-    // Check if the authenticated user has the 'hospital' role
-    if (req.user.role !== 'hospital') {
-      return res.status(403).json({ message: 'Access denied. Hospital authorization required.' });
+    //console.log(decoded);
+    //console.log(decoded.id);
+    
+    const user = await User.findById(decoded.id);
+    
+    if (!user) {
+      throw new Error('User not found');
     }
 
+    req.user = user; 
     next();
-
-  } catch (error) {
-    console.error("Error verifying token:", error);
-    return res.status(403).json({ message: 'Invalid token' });
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({ message: 'Authentication failed', error: err.message });
   }
 };
 
-module.exports = { isAdminOrSuperAdmin, isSuperAdmin, authenticateHospital };
+
+module.exports = { isAdminOrSuperAdmin, isSuperAdmin, authenticate };
