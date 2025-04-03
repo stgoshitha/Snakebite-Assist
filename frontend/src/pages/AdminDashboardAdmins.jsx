bde6aee3031f09c72f48170c273f30d084c3eecc
tharushika2002 <it22296450@my.sliit.lk>
2025-04-03 09:22:11 +0530

    Enhanced with role filters and improved UI

--- frontend/src/pages/AdminDashboardAdmins.jsx
+++ frontend/src/pages/AdminDashboardAdmins.jsx
@@ -2,21 +2,31 @@ import { React, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { get, patch, deleteUser } from "../services/ApiEndpoint";
 import { useNavigate } from "react-router-dom";
+import { Lock, Unlock } from "lucide-react";
+import { CgAddR } from "react-icons/cg";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
-const AdminDashboardUsers = () => {
+const AdminDashboardAdmins = () => {
   const user = useSelector((state) => state.Auth.user);
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
+  const [roleFilter, setRoleFilter] = useState("");
 
   useEffect(() => {
     const getUsers = async () => {
       try {
         const request = await get("/api/admin/getUser");
         const response = request.data.users;
-        //console.log(response);
-        const filteredUsers = response.filter(
+        const filteredAdmins = response.filter(
           (user) => user.role === "superadmin" || user.role === "admin"
         );
+
+        const filteredUsers = filteredAdmins.filter((user) => {
+          const roleMatch = roleFilter ? user.role === roleFilter : true;
+          return roleMatch;
+        });
+
         setUsers(filteredUsers);
       } catch (err) {
         console.error(err);
@@ -24,7 +34,7 @@ const AdminDashboardUsers = () => {
     };
 
     getUsers();
-  }, []);
+  }, [roleFilter]);
 
   const handleBlockUser = async (userId, isBlocked) => {
     try {
@@ -47,13 +57,12 @@ const AdminDashboardUsers = () => {
   };
 
   const handleDeleteUser = async (userId) => {
-    console.log(`Deleting user with ID: ${userId}`);
-  
     try {
       const response = await deleteUser(`/api/admin/deleteUser/${userId}`);
-  
       if (response.status === 200) {
-        setUsers((prevUsers) => prevUsers.filter((user) => user._id !== userId));
+        setUsers((prevUsers) =>
+          prevUsers.filter((user) => user._id !== userId)
+        );
         alert("User has been deleted");
       } else {
         console.error("Failed to delete user:", response.statusText);
@@ -62,65 +71,105 @@ const AdminDashboardUsers = () => {
       console.error("Error deleting user:", err.response?.data || err.message);
     }
   };
-  
 
   return (
     <div className="p-4">
       <h1 className="text-2xl font-semibold mb-4">User Details</h1>
-      {user?.role === "superadmin" && (
-        <div className="mt-4 space-x-4">
+      <div className="flex justify-end items-center mb-4">
+        {user?.role === "superadmin" && (
           <button
             onClick={() => navigate("/admin/allAdmins/addAdminForm")}
-            className="px-4 py-2 mb-3 bg-zinc-600 text-white rounded"
+            className="flex items-center gap-2 px-4 py-2 bg-zinc-600 text-white rounded shadow-md hover:bg-zinc-700 transition duration-300"
           >
-            Create New Admins
+            <CgAddR size={20} /> Create New Admin
           </button>
-        </div>
-      )}
-      <table className="min-w-full table-auto border-collapse bg-white shadow-md rounded-md">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="py-2 px-4 border-b">ID</th>
-            <th className="py-2 px-4 border-b">Name</th>
-            <th className="py-2 px-4 border-b">Email</th>
-            <th className="py-2 px-4 border-b">Role</th>
-            <th className="py-2 px-4 border-b">Status</th>
-            <th className="py-2 px-4 border-b">Actions</th>
-            <th className="py-2 px-4 border-b"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id} className="hover:bg-gray-50">
-              <td className="py-2 px-4 border-b">{user._id}</td>
-              <td className="py-2 px-4 border-b">{user.name}</td>
-              <td className="py-2 px-4 border-b">{user.email}</td>
-              <td className="py-2 px-4 border-b">{user.role}</td>
-              <td className="py-2 px-4 border-b">
-                {user.isBlocked ? "Blocked" : "Active"}
-              </td>
-              <td className="py-2 px-4 border-b">
-                <button
-                  onClick={() => handleBlockUser(user._id, user.isBlocked)}
-                  className={`px-4 py-2 rounded-md text-white ${
-                    user.isBlocked ? "bg-green-600" : "bg-red-600"
-                  } hover:bg-opacity-80`}
-                >
-                  {user.isBlocked ? "Unblock" : "Block"}
-                </button>
-              </td>
-              <td className="py-2 px-4 border-b"><button
-                  onClick={() => handleDeleteUser(user._id)}
-                  className="px-4 py-2 rounded-md text-white bg-red-500 hover:bg-red-600"
-                >
-                  Delete
-                </button></td>
+        )}
+      </div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full table-auto border-collapse bg-white shadow-md rounded-md">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="py-2 px-4 border-b text-left">Name</th>
+              <th className="py-2 px-4 border-b text-left">Email</th>
+              <th className="py-2 px-4 border-b text-left">
+                <div className="flex gap-1 justify-left items-center">
+                  <label className="mr-2">Role </label>
+                  <select
+                    value={roleFilter}
+                    onChange={(e) => setRoleFilter(e.target.value)}
+                    className="p-2 w-48 border rounded-md"
+                  >
+                    <option value="">All Roles</option>
+                    <option value="superadmin">Super Admin</option>
+                    <option value="admin">Admin</option>
+                  </select>
+                </div>
+              </th>
+              <th className="py-2 px-4 border-b">Access Status</th>
+              <th className="py-2 px-4 border-b">Change Access</th>
+              <th className="py-2 px-4 border-b">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id} className="hover:bg-gray-50">
+                <td className="py-2 px-4 border-b">{user.name}</td>
+                <td className="py-2 px-4 border-b">{user.email}</td>
+                <td className="py-2 px-4 border-b">{user.role}</td>
+                <td className="py-2 px-4 border-b">
+                  <div className="flex justify-center items-center">
+                    <div
+                      className={`text-center w-20 rounded-2xl font-semibold border ${
+                        user.isBlocked
+                          ? "bg-red-100 text-red-600 border-none"
+                          : "bg-green-100 text-green-600 border-none"
+                      }`}
+                    >
+                      {user.isBlocked ? "Blocked" : "Active"}
+                    </div>
+                  </div>
+                </td>
+                <td className="py-2 px-4 border-b text-center">
+                  <div className="flex justify-center items-center">
+                    <button
+                      onClick={() => handleBlockUser(user._id, user.isBlocked)}
+                      className={`flex items-center gap-2 px-4 py-2 w-28 rounded-md text-white font-medium shadow-md ${
+                        user.isBlocked
+                          ? "bg-green-600 hover:bg-green-500"
+                          : "bg-red-600 hover:bg-red-500"
+                      }`}
+                    >
+                      {user.isBlocked ? (
+                        <Unlock size={18} />
+                      ) : (
+                        <Lock size={18} />
+                      )}
+                      {user.isBlocked ? "Unblock" : "Block"}
+                    </button>
+                  </div>
+                </td>
+                <td className="py-2 px-4 border-b">
+                  <div className="flex gap-2 justify-center">
+                    <button
+                      className="px-4 py-2 rounded-md text-white bg-green-500 hover:bg-green-600"
+                    >
+                      <FaEdit size={20} />
+                    </button>
+                    <button
+                      onClick={() => handleDeleteUser(user._id)}
+                      className="px-4 py-2 rounded-md text-white bg-red-500 hover:bg-red-600"
+                    >
+                      <MdDelete size={20} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
-export default AdminDashboardUsers;
+export default AdminDashboardAdmins;
