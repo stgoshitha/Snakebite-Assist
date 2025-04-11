7d86dcadfb8c59bd294ab69a21580d6dc6331bf9
tharushika2002 <it22296450@my.sliit.lk>
2025-04-11 10:36:53 +0530

    Add button to navigate user blog page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -68,9 +68,15 @@ const Home = () => {
       )}
 
       {user?.role === "user" && (
-        <div>
+        <div className="flex flex-col justify-center items-center">
           <div className="flex justify-center items-center gap-2 text-2xl mb-3 "><img src={userProfile} className="w-15"/>{user.email} </div>
           <AutocompleteSearch />
+          <button
+            onClick={() => navigate("/userBlog")}
+            className="px-4 py-2 bg-zinc-600 text-white rounded"
+          >
+            My Blog
+          </button>
         </div>
         
       )}
